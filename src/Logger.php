7d3a730e8a99f78db95cf98cfa4b52fe90f43fb9
atanvarno <atanvarno69@gmail.com>
2019-04-18 15:13:38 +0100

    Update interface description and thrown tags

--- src/Logger.php
+++ src/Logger.php
@@ -14,39 +14,32 @@ namespace Pls\Log;
 interface Logger
 {
     /**
-     * Logs a $message with an arbitrary RFC 5424 $level, optionally with a
-     * $context.
+     * Logs a $message with an arbitrary severity $level, optionally with a
+     * $context array.
      *
-     * $message MAY contain placeholders which implementors MAY replace with
-     * values from the $context array. Placeholder names MUST correspond to keys
-     * in the context array. Placeholder names MUST be delimited with a single
-     * opening brace `{` and a single closing brace `}`. There MUST NOT be any
-     * whitespace between the delimiters and the placeholder name. Placeholder
-     * names SHOULD be composed only of the characters `A-Z`, `a-z`, `0-9`,
-     * underscore `_`, and dot `.`. The use of other characters is reserved for
-     * future modifications of the placeholders specification.
+     * @param int    $level   The severity level of the message; One of the
+     *     eight RFC 5424 levels (debug, info, notice, warning, error, critical, 
+     *     alert, emergency). It is RECOMMENDED to pass one of the `LogLevel`
+     *     constants as this parameter value.
+     * @param string $message Message to be logged. The message MAY contain
+     *     placeholders which implementors MUST replace with values from the
+     *     $context array. Placeholder names MUST correspond to keys in the
+     *     context array. Placeholder names MUST be delimited with a single
+     *     opening brace `{` and a single closing brace `}`. There MUST NOT be
+     *     any whitespace between the delimiters and the placeholder name.
+     *     Placeholder names MUST be composed only of the characters `A-Z`,
+     *     `a-z`, `0-9`, underscore `_`, and dot `.`.
+     * @param array  $context Extraneous information to be logged. A given value
+     *     in the context MUST NOT throw an exception nor raise any PHP error,
+     *     warning or notice. If an object implementing `\Throwable` is passed 
+     *     in the context data, it MUST be in the `'thrown'` key. Implementors
+     *     MUST verify that the `'thrown'` key is actually a `\Throwable` before
+     *     using it as such, as it MAY contain anything.
      *
-     * $context can contain anything. Implementors MUST ensure they treat
-     * context data with as much lenience as possible. A given value in $context
-     * MUST NOT throw an exception nor raise any php error, warning or notice.
-     * If a `Throwable` object is passed in the context data, it MUST be in the
-     * 'exception' key. Logging exceptions is a common pattern and this allows
-     * implementors to extract a stack trace from the exception when the log
-     * backend supports it. Implementors MUST still verify that the 'exception'
-     * key is actually a `Throwable` before using it as such, as it MAY contain
-     * anything.
-     *
-     * @param int    $level   One of the eight RFC 5424 levels (debug, info,
-     *     notice, warning, error, critical, alert, emergency). It is
-     *     RECOMMENDED to pass one of the `LogLevel` constants as this parameter
-     *     value.
-     * @param string $message Message to be logged which MAY container
-     *     placeholders
-     * @param array  $context Extraneous information to be logged that does not
-     *     fit well in a string.
-     *
-     * @throws LogException MUST be thrown if a given parameter is an illegal
-     *     value. This method MUST NOT throw any other exceptions.
+     * @throws InvalidContext  $context is missing a value for a placeholder in
+     *     $message.
+     * @throws InvalidMessage  $message contains invalid placeholder names.
+     * @throws InvalidSeverity $level is not >= 0 and <= 7.
      *
      * @return void
      */
