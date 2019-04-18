25b8c2439d9ae25e68443662067cc9011236acd2
atanvarno <atanvarno69@gmail.com>
2019-04-18 15:18:55 +0100

    Create InvalidMessage.php

---
+++ src/InvalidMessage.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package   Pls\Log
+ * @author    PHP Library Standards <https://github.com/PHP-library-standards>
+ * @copyright 2019 PHP Library Standards
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+namespace Pls\Log;
+
+/**
+ * Interface for invalid message exceptions.
+ */
+interface InvalidMessage extends LoggerException
+{
+}
